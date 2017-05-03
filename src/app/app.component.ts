63cc582a8ca18c0efedc83c76ea47c66da7e1187
Brian Weber <brian.weber1337@gmail.com>
2017-05-03 11:00:11 -0600

    removed quickstart her name reference and moved to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,20 @@ export class Hero {
   name: string;
 }
 
-//array of heroes
+//array of heroes. Commented names are names of heroes from tutorial.
 const HEROES: Hero[] = [
-  
+  { id: 2, name: 'Hephaestos'}, //Mr. Nice
+  { id: 3, name: 'Eros' },      //Narco
+  { id: 4, name: 'Dionysos' },  //Bombasto
+  { id: 5, name: 'Achilles' },  //Celeritas
+  { id: 6, name: 'Athena' },    //Magneta
+  { id: 7, name: 'Poseidon' },  //RubberMan
+  { id: 8, name: 'Artemis' },   //Dynama
+  { id: 9, name: 'Apollo' },    //Dr IQ
+  { id: 10, name: 'Heracles' }, //Magma
+  { id: 11, name: 'Hermes' },   //Tornado
+  { id: 12, name: 'Hera' },
+  { id: 13, name: 'Zeus' }
 ]
 
 //so this is called a decorator?
@@ -48,6 +59,6 @@ export class AppComponent  {
   //name
   hero: Hero = {
     id: 1,
-    name: 'Aphrodite'
+    name: 'Aphrodite' //Windstorm
   }
 }

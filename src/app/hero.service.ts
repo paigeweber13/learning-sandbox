17fead417f2e7ef6ac8114091bf6b5283d3931e4
Brian Weber <brian.weber1337@gmail.com>
2017-05-18 15:18:36 -0600

    moved getheroesslowly next to getheroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,6 +15,12 @@ export class HeroService {
     getHeroes(): Promise<Hero[]> {
         return Promise.resolve(HEROES);
     };
+    getHeroesSlowly(): Promise<Hero[]> {
+        return new Promise(resolve => {
+            //Simulate server lateny with 2 second delay
+            setTimeout(() => resolve(this.getHeroes()), 2000);
+        });
+    }
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
             .then(heroes => heroes.find(
@@ -27,10 +33,4 @@ export class HeroService {
                 function (hero) { return hero.id === id; }); });
         */
     };
-    getHeroesSlowly(): Promise<Hero[]>{
-        return new Promise(resolve => {
-            //Simulate server lateny with 2 second delay
-            setTimeout(() => resolve(this.getHeroes()), 2000);
-        });
-    }
 }

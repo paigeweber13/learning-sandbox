7a3995a97d00e6dab518b8d5a8c49688f4714cbe
Brian Weber <brian.weber1337@gmail.com>
2017-05-26 11:34:59 -0600

    Added get hero by id, update, and create functionality

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,6 +12,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
     private heroesUrl = 'api/heroes'; // URL to web api
+    private headers = new Headers({ 'Content-Type': 'application/json' });
 
     constructor(private http: Http) {}
 
@@ -31,21 +32,46 @@ export class HeroService {
             setTimeout(() => resolve(this.getHeroes()), 2000);
         });
     }
-    getHero(id: number): Promise<Hero> {
-        return this.getHeroes()
-            .then(heroes => heroes.find(
-                hero => hero.id === id));
-
-        // Javascript equivalent:
-        /*
-        return this.getHeroes()
-            .then(function (heroes) { return heroes.find(
-                function (hero) { return hero.id === id; }); });
-        */
-    };
+    getHero(id: number): Promise < Hero > {
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get(url)
+            .toPromise()
+            .then(response => response.json().data as Hero)
+            .catch(this.handleError);
+    }
 
     private handleError(error: any): Promise<any> {
         console.error('An error occured', error); // for demo purposes only
         return Promise.reject(error.message || error);
     }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), { headers: this.headers })
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    create(name: String): Promise<Hero> {
+        return this.http
+            .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+            .toPromise()
+            .then(res => res.json().data as Hero)
+            .catch(this.handleError);
+    }
+
+    // Typescript vs. Javascript
+    /*
+    return this.getHeroes()
+        .then(heroes => heroes.find(
+            hero => hero.id === id));
+
+    // Javascript equivalent:
+    
+    return this.getHeroes()
+        .then(function (heroes) { return heroes.find(
+            function (hero) { return hero.id === id; }); });
+    */
 }

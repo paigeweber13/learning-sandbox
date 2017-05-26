6020ef8228c29ee926032837351dbe681686e656
Brian Weber <brian.weber1337@gmail.com>
2017-05-26 13:17:25 -0600

    added images

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,19 +4,19 @@ export class InMemoryDataService implements InMemoryDbService {
     createDb() {
         // * array of heroes. Commented names are names of heroes from tutorial.
         let heroes = [
-            { id: 1, name: 'Aphrodite' },  // Windstorm
-            { id: 2, name: 'Hephaestos' }, // Mr. Nice
-            { id: 3, name: 'Eros' },       // Narco
-            { id: 4, name: 'Dionysos' },   // Bombasto
-            { id: 5, name: 'Achilles' },   // Celeritas
-            { id: 6, name: 'Athena' },     // Magneta
-            { id: 7, name: 'Poseidon' },   // RubberMan
-            { id: 8, name: 'Artemis' },    // Dynama
-            { id: 9, name: 'Apollo' },     // Dr IQ
-            { id: 10, name: 'Heracles' },  // Magma
-            { id: 11, name: 'Hermes' },    // Tornado
-            { id: 12, name: 'Hera' },
-            { id: 13, name: 'Zeus' }
+            { id: 1,  name: 'Aphrodite',  imgFullPath: '/assets/aphrodite.png'  }, // Windstorm
+            { id: 2,  name: 'Hephaestos', imgFullPath: '/assets/hephaestos.png' }, // Mr. Nice
+            { id: 3,  name: 'Eros',       imgFullPath: '/assets/eros.png'       }, // Narco
+            { id: 4,  name: 'Dionysos',   imgFullPath: '/assets/dionysos.png'   }, // Bombasto
+            { id: 5,  name: 'Achilles',   imgFullPath: '/assets/achilles.png'   }, // Celeritas
+            { id: 6,  name: 'Athena',     imgFullPath: '/assets/athena.png'     }, // Magneta
+            { id: 7,  name: 'Poseidon',   imgFullPath: '/assets/poseidon.png'   }, // RubberMan
+            { id: 8,  name: 'Artemis',    imgFullPath: '/assets/artemis.png'    }, // Dynama
+            { id: 9,  name: 'Apollo',     imgFullPath: '/assets/apollo.png'     }, // Dr IQ
+            { id: 10, name: 'Heracles',   imgFullPath: '/assets/heracles.png'   }, // Magma
+            { id: 11, name: 'Hermes',     imgFullPath: '/assets/hermes.png'     }, // Tornado
+            { id: 12, name: 'Hera',       imgFullPath: '/assets/hera.png'       },
+            { id: 13, name: 'Zeus',       imgFullPath: '/assets/zeus.png'       }
         ];
         return { heroes };
     }

c456e1c0aa8deffd6104b3c7621a2149ec66786a
Brian Weber <brian.weber1337@gmail.com>
2017-05-26 11:04:14 -0600

    replaced mock-heroes with mock http service

---
+++ src/app/in-memory-data.service.ts
@@ -0,0 +1,22 @@
+import { InMemoryDbService } from 'angular-in-memory-web-api';
+export class InMemoryDataService implements InMemoryDbService {
+    createDb() {
+        // * array of heroes. Commented names are names of heroes from tutorial.
+        let heroes = [
+            { id: 1, name: 'Aphrodite' },  // Windstorm
+            { id: 2, name: 'Hephaestos' }, // Mr. Nice
+            { id: 3, name: 'Eros' },       // Narco
+            { id: 4, name: 'Dionysos' },   // Bombasto
+            { id: 5, name: 'Achilles' },   // Celeritas
+            { id: 6, name: 'Athena' },     // Magneta
+            { id: 7, name: 'Poseidon' },   // RubberMan
+            { id: 8, name: 'Artemis' },    // Dynama
+            { id: 9, name: 'Apollo' },     // Dr IQ
+            { id: 10, name: 'Heracles' },  // Magma
+            { id: 11, name: 'Hermes' },    // Tornado
+            { id: 12, name: 'Hera' },
+            { id: 13, name: 'Zeus' }
+        ];
+        return { heroes };
+    }
+}

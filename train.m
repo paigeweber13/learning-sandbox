89472adab9fe38450049495a2a364637cabada46
Riley Weber <rileyw13@protonmail.com>
2019-03-06 10:35:53 -0500

    no longer using matlab

--- train.m
+++
@@ -1,6 +0,0 @@
-trainingImages = fopen(train-images-idx3-ubyte);
-numImg = 60000;
-imgWidth = 28;
-imgHeight
-rawImageDataTrain = uint8 (fread(trainingImages, ...
-    numImg * numRows * numCols, 'unit8'));

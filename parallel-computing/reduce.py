6ce381fadb22f8646dc90e4f8b65a10e83f99ebe
Riley Weber <rileyw13@protonmail.com>
2019-06-06 16:29:54 -0400

    added sequentail sum and corresponding test

--- parallel-computing/reduce.py
+++ parallel-computing/reduce.py
@@ -2,3 +2,12 @@ import multiprocessing
 import threading
 
 NUM_CPUS = multiprocessing.cpu_count()
+
+def sum_array(array_to_sum):
+    sum = 0
+    for x in array_to_sum:
+        sum += x
+    return sum
+
+def sum_array_static_parallel(array_to_sum):
+    pass
--- parallel-computing/test-reduce.py
+++ parallel-computing/test-reduce.py
@@ -1,5 +1,12 @@
 import unittest
 
+import reduce
+
 class TestReduce(unittest.TestCase):
     def test_thing(self):
         self.assertTrue(True)
+    
+    def test_sequential_sum(self):
+        expected = 20
+        actual = reduce.sum_array([1,0,16,-3,6])
+        self.assertEqual(expected, actual)

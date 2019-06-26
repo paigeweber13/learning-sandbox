9c71689fc246c9f09c6e35dd7a52960cc2571f98
Riley Weber <brian.weber@sealedair.com>
2019-06-26 08:33:20 -0400

    added display of delta to understand how it works

--- godot/godot-docs-tutorial/sayHello.gd
+++ godot/godot-docs-tutorial/sayHello.gd
@@ -1,7 +1,18 @@
 extends Panel
 
+var accum = 0
+var accum2 = 0
+
 func _ready() -> void:
 	get_node("Button").connect("pressed", self, "_on_Button_pressed")
 	
 func _on_Button_pressed() -> void:
 	get_node("Label").text = "Hello!"
+
+func _process(delta) -> void:
+	accum += delta
+	get_node("Label2").text = "Process accumulated time: " + str(accum)
+
+func _physics_process(delta) -> void:
+	accum2 += delta
+	get_node("Label3").text = "Physics accumulated time: " + str(accum2)

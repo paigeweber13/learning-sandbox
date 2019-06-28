3bf6617642d59244fd527b3cbf0e09dd4758f975
Riley Weber <rileyw13@protonmail.com>
2019-06-28 10:37:37 -0400

    now connects via code as an exercise

--- godot/godot-docs-tutorial/TimerExample.gd
+++ godot/godot-docs-tutorial/TimerExample.gd
@@ -6,6 +6,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Timer.connect("timeout", self, "_on_Timer_timeout")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

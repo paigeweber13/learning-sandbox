df9eee3cdb30be6aed2a572c1c283bfef80fa1fb
Riley <rileyw13@protonmail.com>
2019-09-06 16:56:11 -0400

    now dynamically imports based on command line parameters

--- artificial-intelligence-practice/openai-gym/cli.py
+++ artificial-intelligence-practice/openai-gym/cli.py
@@ -15,22 +15,31 @@ from gym import wrappers, logger
 # expects file containing agent to have name foo_bar.py and class inside that
 # file to be named FooBar
 def import_recursive(root_dir: str, package: str):
+    imported_module = None
     for dir_info in os.walk(root_dir):
         if 'results' not in dir_info[0]:
-            print()
-            print(dir_info[0])
+            # print()
+            # print(dir_info[0])
             for (_, name, _) in pkgutil.iter_modules([Path(dir_info[0])]):
-                print(name)
+                # print(name)
                 if package in name:
                     name = '.' + name
-                    package = dir_info[0][2:].replace('/', '.')
-                    print('attempting import: import_module(' + name + ', ' + package +')')
-                    imported_module = import_module(name, package)
-                    print('imported module:', imported_module)
-                    return
+                    thing_to_import = dir_info[0][2:].replace('/', '.')
+                    # print('attempting import: import_module(' + name + ', ' + thing_to_import +')')
+                    imported_module = import_module(name, thing_to_import)
+                    # print('imported module:', imported_module)
+                    break
                 # print(name)
 
-    
+    print('imported module:', imported_module)
+    class_name = ''
+    for word in package.split('_'):
+        class_name += word.capitalize()
+
+    for part_of_package in dir(imported_module):
+        if class_name in part_of_package:
+            # found the class! Returning...
+            return getattr(imported_module, part_of_package)
 
     # for (_, name, _) in pkgutil.iter_modules([Path(__file__).parent]):
     # for (_, name, _) in pkgutil.iter_modules([os.path.dirname(__file__)]):
@@ -52,8 +61,10 @@ def main():
     # if you want outdir to be cleared every run, make force=True
     env = wrappers.Monitor(env, directory=outdir, force=False)
     env.seed(0)
+
     # TODO: replace programatically
-    agent = random_agent.RandomAgent(env.action_space)
+    input_class = import_recursive('.', args.agent)
+    agent = input_class(env.action_space)
 
     episode_count = int(args.episode_count)
     reward = 0
@@ -72,5 +83,4 @@ def main():
     env.close()
 
 if __name__ == '__main__':
-    import_recursive('.', 'random_agent')
-    # main()
+    main()

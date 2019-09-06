21158df12b116192755568b6e7366edd9813e254
Riley <rileyw13@protonmail.com>
2019-09-06 16:41:13 -0400

    minor change for readibility

--- artificial-intelligence-practice/openai-gym/examples/agents/random_agent.py
+++ artificial-intelligence-practice/openai-gym/examples/agents/random_agent.py
@@ -38,11 +38,9 @@ if __name__ == '__main__':
 
     for i in range(episode_count):
         ob = env.reset()
-        while True:
+        while not done:
             action = agent.act(ob, reward, done)
             ob, reward, done, _ = env.step(action)
-            if done:
-                break
             # Note there's no env.render() here. But the environment still can open window and
             # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
             # Video is not recorded every episode, see capped_cubic_video_schedule for details.

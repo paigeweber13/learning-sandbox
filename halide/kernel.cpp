16f188a7c667c64c601cca42633a91094ed68321
Riley <rileyw13@protonmail.com>
2019-10-06 19:57:17 -0400

    changed half to midpoint, removed this because I found out it's unnecessary

--- halide/kernel.cpp
+++ halide/kernel.cpp
@@ -12,12 +12,14 @@ Kernel::Kernel(size_t k){
   }
 
   for (size_t i = 0; i < k; i++){
-    this->values.push_back(vector<float>(k));
+    values.push_back(vector<float>(k));
   }
 
   this->k = k;
-  this->midpoint = size_t(ceil(k/2));
-  this->half = midpoint - 1;
+  // cout << "k: " << this->k << endl;
+  midpoint = size_t(ceil(k/2));
+  // this->half = midpoint - 1;
+  // cout << this->to_string() << endl;
 }
 
 Kernel::~Kernel(){
@@ -35,12 +37,15 @@ string Kernel::to_string(){
   // todo: does setprecision do anything?
   // setprecision(4);
   stringstream ss;
-  ss << "k: " << this->k << endl;
-  ss << "Midpoint: " << this->midpoint << endl;
-  ss << "Half: " << this->half << endl;
+  // cout << "k: " << k << endl;
+  ss << "k: " << k << endl;
+  // cout << "midpoint: " << midpoint << endl;
+  ss << "Midpoint: " << midpoint << endl;
+  // ss << "Half: " << this->half << endl;
   ss << "Kernel:" << endl;
-  for(int x = -half; x <= half; x++){
-    for(int y = half; y >= -half; y--){
+  for(int x = -midpoint; x <= midpoint; x++){
+    for(int y = midpoint; y >= -midpoint; y--){
+      // cout << "(x, y): " << x << ", " << y << endl;
       ss << get(x, y) << "  ";
     }
     ss << endl;

2e919086196e8680d171c0f35a77bb91ffd2305c
Riley <rileyw13@protonmail.com>
2019-10-06 18:52:06 -0400

    added command line parsing, functionality for blur

--- halide/test.cpp
+++ halide/test.cpp
@@ -1,14 +1,19 @@
+#include <iostream>
+#include <string>
+
+#include "halide_blur.h"
+
 // based on this tutorial: 
 // https://halide-lang.org/tutorials/tutorial_lesson_02_input_image.html
 
 // The only Halide header file you need is Halide.h. It includes all of Halide.
 #include "Halide.h"
 
-// Include some support code for loading pngs.
+// Include some support code for loading jpgs.
 #include "halide_image_io.h"
 using namespace Halide::Tools;
 
-int main(int argc, char **argv) {
+void brighten(){
   // load an image
   Halide::Buffer<uint8_t> input = load_image("saturn-v-2048x2048-bw.jpg");
 
@@ -25,6 +30,29 @@ int main(int argc, char **argv) {
 
   // Save the output for inspection. It should look like a bright rocket.
   save_image(output, "out/brighter.png");
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2){
+    cout << "usage: " << argv[0] << " brighten|blur [kernel_size]" << endl;
+    return 1;
+  }
+
+  size_t kernel_size;
+  if (argc > 2){
+    kernel_size = size_t(atoi(argv[2]));
+  } else {
+    kernel_size = 3;
+  }
+
+  if (argv[1] == "brighten"){
+    brighten();
+  } else if (argv[1] == "blur"){
+    blur(kernel_size, "saturn-v-2048x2048-bw.jpg");
+  } else {
+    cout << "invalid mode, choose \"brighten\" or \"blur\"." << endl;
+    return 2;
+  }
 
   printf("Success!\n");
   return 0;

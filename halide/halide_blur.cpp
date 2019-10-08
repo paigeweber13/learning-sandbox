4435c9de4e4dfe2c12b72c7efc45d53de82c9f65
Riley <rileyw13@protonmail.com>
2019-10-07 21:15:47 -0400

    trying to get halide to apply convolution now

--- halide/halide_blur.cpp
+++ halide/halide_blur.cpp
@@ -9,32 +9,31 @@ void blur(size_t kernel_size, std::string input_image_filename){
     std::cout << kernels[i].to_string();
   }
 
-  /*
   Halide::Buffer<uint8_t> input = load_image(input_image_filename);
 
-  Halide::Var x, y;
+  Halide::Var x, y, k;
   Halide::Func blur;
 
-  auto convolution_matrix = generate_kernel(kernel_size);
+  auto convolution_matrix = generate_blur_kernel(kernel_size);
 
   // Func will equal value and value gets applied to every pixel (of location
   // (x,y)) in the image.
   Halide::Expr value = Halide::cast<float>(input(x, y));
   // numbering below is confusing because matrix is accessed with [row][column]
   // and x and y are like a cartesian plane
-  value = input(x-1, y-1) * convolution_matrix[0][0]
-        + input(x  , y-1) * convolution_matrix[0][0]
-        + input(x+1, y-1) * convolution_matrix[0][0]
-        + input(x-1, y  ) * convolution_matrix[0][0]
-        + input(x  , y  ) * convolution_matrix[0][0]
-        + input(x+1, y  ) * convolution_matrix[0][0]
-        + input(x-1, y+1) * convolution_matrix[0][0]
-        + input(x  , y+1) * convolution_matrix[0][0]
-        + input(x+1, y+1) * convolution_matrix[0][0]
+  // for(int i = 0; i <= k; i++)
+  // value = input(x-1, y-1) * convolution_matrix[0][0]
+  //       + input(x  , y-1) * convolution_matrix[0][0]
+  //       + input(x+1, y-1) * convolution_matrix[0][0]
+  //       + input(x-1, y  ) * convolution_matrix[0][0]
+  //       + input(x  , y  ) * convolution_matrix[0][0]
+  //       + input(x+1, y  ) * convolution_matrix[0][0]
+  //       + input(x-1, y+1) * convolution_matrix[0][0]
+  //       + input(x  , y+1) * convolution_matrix[0][0]
+  //       + input(x+1, y+1) * convolution_matrix[0][0]
 
-  blur(x, y)
+  blur(x, y) = value;
 
   Halide::Buffer<uint8_t> output =
     blur.realize(input.width(), output.width());
-  */
 }

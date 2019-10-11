f540ac85b49df247a666bc4f931c416fa3a074d0
Riley Weber <rileyw13@protonmail.com>
2019-10-11 11:14:58 -0400

    added function that recursively doubles list and demoed usage

--- haskell-demo-programs/DataStructuresDemo.hs
+++ haskell-demo-programs/DataStructuresDemo.hs
@@ -1,9 +1,13 @@
 module Main where
 
+doubleList :: Num a => [a] -> [a]
+doubleList [] = []
+doubleList (x:xs) = (x * 2) : doubleList xs
+
 main =
   do
-    -- Haskell primarily uses Lists and Tuples to handle data.
-    print "doing basic list operations"
+    putStrLn "Haskell primarily uses Lists and Tuples to handle data."
+    putStrLn "doing basic list operations"
     let a = [1, 2, 3] :: [Int]
     let b = [9, 8, 7] :: [Int]
     putStrLn ("a: " ++ show a)
@@ -13,8 +17,16 @@ main =
     putStrLn ("adding another number to that list: " ++ show (19:c))
     putStrLn ""
 
-    -- All items in a list must be the same type. Tuples, however, may have
-    -- elements of different types
-    putStrLn "doing basic tuple operations"
+    putStrLn "All items in a list must be the same type. Tuples, however, may"
+    putStrLn "have elements of different types. For example:"
     let coordinate = ("cartesian", 2, 5)
     putStrLn ("coordinate: " ++ show coordinate)
+    putStrLn "tuples are immutable, so there isn't much we can do here..."
+
+    putStrLn "here are some operations on lists:"
+    let d = [1, 0, -61, 43]
+    putStrLn "we can use recursion to double all elements in a list:"
+    let e = doubleList d
+    putStrLn ("original list: " ++ show d)
+    putStrLn ("doubled list: " ++ show e)
+

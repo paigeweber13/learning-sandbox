5c39a12069f49f6bdf365c886979dd08e2ed09ca
Riley Weber <rileyw13@protonmail.com>
2019-10-23 23:50:51 -0400

    added basic fahr/celsius code

---
+++ c-programming-knr-book/temp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// if you don't explicitly declare a return type, it defaults to int
+main() {
+  // memory allocation on the stack happens here
+  int fahr, celsius;
+  int lower, upper, step;
+
+  // writing values to memmory happens here
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+  while (fahr <= upper){
+    celsius = 5 * (fahr - 32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}

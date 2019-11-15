f71b6bea587333a25281cb90a34c0ec5889bd4ac
Riley Weber <rileyw13@protonmail.com>
2019-11-15 11:52:00 -0500

    added boost program options, a compiled portion of boost

--- cpp-package-manager-experiments/conan/md5.cpp
+++ cpp-package-manager-experiments/conan/md5.cpp
@@ -1,15 +1,44 @@
- #include "Poco/MD5Engine.h"
- #include "Poco/DigestStream.h"
+#include <boost/program_options.hpp>
+#include "Poco/MD5Engine.h"
+#include "Poco/DigestStream.h"
 
- #include <iostream>
+#include <iostream>
+
+namespace po = boost::program_options;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  // ---- boost program options
+  // Declare the supported options.
+  po::options_description desc("Allowed options");
+  desc.add_options()
+    ("help", "produce help message")
+    ("compression", po::value<int>(), "set compression level")
+  ;
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, desc), vm);
+  po::notify(vm);    
+
+  if (vm.count("help")) {
+    cout << desc << "\n";
+    return 1;
+  }
+
+  if (vm.count("compression")) {
+    cout << "Compression level was set to " 
+         << vm["compression"].as<int>() << ".\n";
+  } else {
+    cout << "Compression level was not set.\n";
+  }
 
 
- int main(int argc, char** argv)
- {
-     Poco::MD5Engine md5;
-     Poco::DigestOutputStream ds(md5);
-     ds << "abcdefghijklmnopqrstuvwxyz";
-     ds.close();
-     std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
-     return 0;
- }
+  // ---- poco md5
+  Poco::MD5Engine md5;
+  Poco::DigestOutputStream ds(md5);
+  ds << "abcdefghijklmnopqrstuvwxyz";
+  ds.close();
+  cout << Poco::DigestEngine::digestToHex(md5.digest()) << endl;
+  return 0;
+}

027a5777c4d1c7158462a5d5b25b2963d6eb6399
Riley Weber <rileyw13@protonmail.com>
2019-11-15 11:25:14 -0500

    deleted rails stuff
    Has security vulnerabilities, I don't have interest in rails any more

--- learning-rails/moviestore/config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  root 'movies#index'
-  resources :movies, only: [:show, :index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end

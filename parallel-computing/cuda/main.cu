237208fca985a24aa71afe9467defabfc06f6a44
Riley Weber <rileyw13@protonmail.com>
2019-11-20 21:57:09 -0500

    trying passing scalar

--- parallel-computing/cuda/main.cu
+++ parallel-computing/cuda/main.cu
@@ -2,10 +2,11 @@
 
 #define ARR_SIZE 10
 
-void __global__ sum_array(int* d_arr, int* res){
+void __global__ sum_array(int* d_arr, int* res, size_t param){
   printf("inside sum_array\n");
+  printf("param: %lu\n", param);
   for(int i = 0; i < ARR_SIZE; i++){
-    res += d_arr[i];
+    *res += d_arr[i];
   }
 }
 
@@ -13,6 +14,7 @@ int main(){
   int arr[ARR_SIZE] = {1, 0, 3, 5, 0, 0, 0, 6, 7, 10};
   int expected_sum = 32;
   int actual_sum = 0;
+  size_t number = 13;
 
   int *d_sum, *d_arr;
 
@@ -20,7 +22,7 @@ int main(){
   cudaMalloc(&d_arr, sizeof(int)*ARR_SIZE);
   cudaMemcpy(d_sum, &actual_sum, sizeof(int), cudaMemcpyDefault);
   cudaMemcpy(d_arr, &arr, sizeof(int)*ARR_SIZE, cudaMemcpyDefault);
-  sum_array<<<1, 1>>>(d_arr, d_sum);
+  sum_array<<<1, 1>>>(d_arr, d_sum, number);
   cudaDeviceSynchronize();
   cudaMemcpy(&actual_sum, d_sum, sizeof(int), cudaMemcpyDefault);
 

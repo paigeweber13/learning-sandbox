c7cfd04b00f06940780bd8fcaf93ea5bff32b571
Riley Weber <rileyw13@protonmail.com>
2020-01-06 12:34:25 -0500

    user can now actually get feedback, loops until correct number is guessed, strings that aren't valid numbers are handled

--- rust-book/guessing_game/src/main.rs
+++ rust-book/guessing_game/src/main.rs
@@ -1,16 +1,35 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-    println!("guess the number!");
+    println!("guess the number, between 1 and 100!");
 
     let secret_number = rand::thread_rng().gen_range(1, 100);
     println!("the secret number is: {}", secret_number);
 
-    println!("input your guess...");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("input your guess...");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("please input a number!");
+                continue;
+            }
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }

e7035b9017c58136edd7ad3ea6c2ba8e6b5c1b27
Riley <rileyw13@protonmail.com>
2020-01-10 18:49:42 -0500

    added exercise on display

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,12 +1,17 @@
 mod formatted_print;
 mod debug;
+mod display;
 
 fn main() {
     println!("formatted print demo:");
     formatted_print::formatted_print();
     println!();
 
-    println!("formatted print demo:");
+    println!("debug derivation demo:");
     debug::debug();
+    println!();
+
+    println!("display implementation demo:");
+    display::display();
     println!();
 }

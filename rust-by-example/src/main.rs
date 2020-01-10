91e822511b6cec88883dd69d8a6ceaed0923b95f
Riley <rileyw13@protonmail.com>
2020-01-10 18:38:13 -0500

    added section on debug

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,5 +1,12 @@
 mod formatted_print;
+mod debug;
 
 fn main() {
+    println!("formatted print demo:");
     formatted_print::formatted_print();
+    println!();
+
+    println!("formatted print demo:");
+    debug::debug();
+    println!();
 }

3913e0d0efcd9cba2ba87fd2d2eb97367218d645
Riley Weber <rileyw13@protonmail.com>
2020-01-16 17:48:20 -0500

    moved ch1 stuff to own folder, optionally runs

--- rust-by-example/src/main.rs
+++ rust-by-example/src/main.rs
@@ -1,27 +1,57 @@
-mod formatted_print;
-mod debug;
-mod display;
-mod testcase_list;
-mod formatting;
+use std::io;
+
+mod ch1 {
+    pub mod formatted_print;
+    pub mod debug;
+    pub mod display;
+    pub mod testcase_list;
+    pub mod formatting;
+}
 
 fn main() {
+    if ask_to_continue("ch1") { ch1_code(); }
+}
+
+fn ask_to_continue(example_info: &str) -> bool {
+    println!("Would you like to run examples from {}?", example_info);
+    let mut input = String::new();
+
+    match io::stdin().read_line(&mut input) {
+        Ok(_) => {
+            if input.trim().to_lowercase() == "y" { 
+                println!("skipping...");
+                true 
+            }
+            else { 
+                println!("skipping...");
+                false
+            }
+        },
+        Err(_) => {
+            println!("error with input, skipping");
+            false
+        },
+    }
+}
+
+fn ch1_code() {
     println!("formatted print demo:");
-    formatted_print::formatted_print();
+    ch1::formatted_print::formatted_print();
     println!();
 
     println!("debug derivation demo:");
-    debug::debug();
+    ch1::debug::debug();
     println!();
 
     println!("display implementation demo:");
-    display::display();
+    ch1::display::display();
     println!();
 
     println!("testcase with list:");
-    testcase_list::testcase_list();
+    ch1::testcase_list::testcase_list();
     println!();
 
     println!("advanced formatting including hex:");
-    formatting::formatting();
+    ch1::formatting::formatting();
     println!();
 }

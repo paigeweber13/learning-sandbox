a074eb36f8bb6bc7e125c0ee2ab0e447e465a023
Riley Weber <rileyw13@protonmail.com>
2020-05-11 09:23:46 -0400

    added function for long-term population growth added more tests for population growth added comments

--- math/bifurcation.jl
+++ math/bifurcation.jl
@@ -19,7 +19,18 @@ end
 
 ### Population growth stuff
 
+# gives population of next year given x (current population) and r (growth
+# rate) 
 population_growth(x, r) = r*x*(1-x)
+
+# gives population after n years, hopefully to find an equilibrium point
+function population_growth_long_term(x, r, n)
+  final_population = x
+  for i = 1:n
+    final_population = population_growth(final_population, r)
+  end
+  final_population
+end
 
 ### Tests
 
@@ -32,13 +43,19 @@ end
 
 function test_population_growth()
   @test population_growth(.4, 2.6) == 0.624
+  @test population_growth(.3, 1.0) == 0.21
   @test decimal_round(population_growth(.2, 1.1), convert(Unsigned, 3)) == 0.176
+  @test decimal_round(population_growth(.374, .7), convert(Unsigned, 4)) == 0.1639
   println("all population_growth tests passed!")
 end
 
-run_tests() = (
+function test_population_growth_long_term()
+  # TODO: implement
+end
+
+function run_tests()
   test_decimal_round();
   test_population_growth();
-)
+end
 
 run_tests()

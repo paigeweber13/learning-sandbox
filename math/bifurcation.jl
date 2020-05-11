6d5dbbfeeb8f926adb10cb25e71a2a5ab1ca2b51
Riley Weber <rileyw13@protonmail.com>
2020-05-11 14:44:56 -0400

    fixed long-term function (now named "series") wrote tests for long term function

--- math/bifurcation.jl
+++ math/bifurcation.jl
@@ -30,9 +30,9 @@ function logistic_map_series(x, r, n, m)
 
   mth_x = nth_x
   series = []
-  for i = (n+1):m
+  for i = 1:m
     mth_x = logistic_map(mth_x, r)
-    series.append(mth_x)
+    append!(series, [mth_x])
   end
   series
 end
@@ -46,21 +46,29 @@ function test_decimal_round()
   println("all decimal_round tests passed!")
 end
 
-function test_population_growth()
+function test_logistic_map()
   @test logistic_map(.4, 2.6) == 0.624
   @test logistic_map(.3, 1.0) == 0.21
   @test decimal_round(logistic_map(.2, 1.1), convert(Unsigned, 3)) == 0.176
   @test decimal_round(logistic_map(.374, .7), convert(Unsigned, 4)) == 0.1639
-  println("all population_growth tests passed!")
+  println("all logistic_map tests passed!")
 end
 
-function test_population_growth_long_term()
-  # TODO: implement
+function test_logistic_map_series()
+  s = logistic_map_series(1, 2, 100, 100)
+  @test size(s, 1) == 100
+
+  # fancy broadcast syntax
+  @test decimal_round.(
+    logistic_map_series(0.77, 1.33, 100, 10), 
+    convert(Unsigned, 3)
+   ) == fill(0.248, (10))
 end
 
 function run_tests()
   test_decimal_round();
-  test_population_growth();
+  test_logistic_map();
+  test_logistic_map_series();
 end
 
 run_tests()

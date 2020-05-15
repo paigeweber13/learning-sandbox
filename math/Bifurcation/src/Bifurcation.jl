03c99606c8c975e0cbd9e7446ef3582384d8c5a8
Riley Weber <rileyw13@protonmail.com>
2020-05-15 16:01:36 -0400

    got type checking to actually work

--- math/Bifurcation/src/Bifurcation.jl
+++ math/Bifurcation/src/Bifurcation.jl
@@ -11,35 +11,35 @@ function main(args)
 
   @add_arg_table! s begin
     "x"
-      arg_type = Real
+      arg_type = Float64
       default = 0.5
       help = "value to be used for first iteration of the logistic map"
 
     "r0"
-      arg_type = Real
+      arg_type = Float64
       default = 0.0
       help = "initial r: the starting point for the plot"
 
     "n"
-      arg_type = Integer
+      arg_type = Int
       default = 100
       range_tester = (n->n>0)
       help = "number of iterations to execute logistic map before result is " *
         "considered to be at equilibrium"
 
     "m"
-      arg_type = Integer
+      arg_type = Int
       default = 100
       range_tester = (m->m>0)
       help = "number of iterations after equilibrium to plot"
 
     "r_inc"
-      arg_type = Real
+      arg_type = Float64
       default = 0.01
       help = "amount to increment r by between tests"
 
     "r_final"
-      arg_type = Real
+      arg_type = Float64
       default = 4.0
       help = "point at which we will stop testing and plotting"
 
@@ -49,7 +49,11 @@ function main(args)
   println("Parsed args:")
   for (key,val) in parsed_args
     println("  $key  =>  $(repr(val))")
-    println("  typeof($key) => $(typeof(repr(val)))")
+    println("  typeof($key) => $(typeof(val))")
+    # println("converting to Float64...")
+    # val = parse(Float64, val)
+    # println("  $key  =>  $(repr(val))")
+    # println()
   end
 end
 

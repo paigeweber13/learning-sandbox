fab1e16c5c2f38d8e4f58af8e2e0b5e07c73e7c3
Riley Weber <rileyw13@protonmail.com>
2021-03-30 12:08:37 -0400

    finds all key info

--- data-science/temperature-logging/log-temps.go
+++ data-science/temperature-logging/log-temps.go
@@ -4,24 +4,56 @@ import (
 	"fmt"
 	"os/exec"
 	"regexp"
+  "time"
 )
 
+type GPUState struct {
+  Id uint
+  Name string
+  Temp int
+  Fan int
+  Power int
+  Memory int
+}
+
 func main() {
-	regexpTemp := regexp.MustCompile(`(\d+)C`)
+  WAIT_TIME := 2 * time.Second
+
+  regexpIdName := regexp.MustCompile(`\+=+\|\n\| +(\d+) +((?:(?:\w+) )+)`)
+  regexpTemp := regexp.MustCompile(`(\d+)C`)
+  regexpFanUsage := regexp.MustCompile(`(\d+)%`)
+  regexpPower := regexp.MustCompile(`(\d+)W /`)
+	regexpMemory := regexp.MustCompile(`(\d+)MiB /`)
 
 	for {
     cmdNvidiaSmi := exec.Command("/usr/bin/nvidia-smi")
-		output, err := cmdNvidiaSmi.Output()
+		outputBytes, err := cmdNvidiaSmi.Output()
 		if err != nil {
 			fmt.Printf("Error running command `%v`\n", cmdNvidiaSmi.String())
 			fmt.Printf("Error was: %v\n", err)
 		}
+    output := string(outputBytes)
 
-		matches := regexpTemp.FindSubmatch(output)
+		matchesIdName := regexpIdName.FindStringSubmatch(output)
+		matchesTemp := regexpTemp.FindStringSubmatch(output)
+		matchesFanUsage := regexpFanUsage.FindAllStringSubmatch(output, 2)
+    matchesFan := matchesFanUsage[0]
+    matchesUsage := matchesFanUsage[1]
+		matchesPower := regexpPower.FindStringSubmatch(output)
+		matchesMemory := regexpMemory.FindStringSubmatch(output)
 
-		fmt.Printf("Got matches!\n")
-		for i, match := range matches {
-      fmt.Printf("Match %v: %v\n", i, string(match))
-		}
+    allMatches := [][]string{
+      matchesIdName, matchesTemp, matchesFan, matchesUsage, matchesPower,
+      matchesMemory}
+
+    for _, matches := range allMatches {
+      fmt.Println("Got matches!")
+			for i, match := range matches {
+        fmt.Printf("Match %v: %v\n", i, match)
+			}
+      fmt.Println()
+    }
+
+    time.Sleep(WAIT_TIME)
 	}
 }

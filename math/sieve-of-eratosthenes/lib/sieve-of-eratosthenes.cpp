6fef6b751ca3dd4ef4423835194d4fb3e56ee5bb
Paige Weber <paigeweber31@gmail.com>
2022-10-19 00:08:59 -0500

    wrote sieve function; all tests pass

--- math/sieve-of-eratosthenes/lib/sieve-of-eratosthenes.cpp
+++ math/sieve-of-eratosthenes/lib/sieve-of-eratosthenes.cpp
@@ -1,5 +1,29 @@
 #include "sieve-of-eratosthenes.hpp"
+#include "vector-utilities.hpp"
 
 std::vector<int> findPrimes(int maxValue){
-  return std::vector<int>();
+  auto primes = std::vector<int>();
+
+  // add all values <= maxValue to array
+  for (int x = 2; x < maxValue + 1; x++) {
+    primes.push_back(x);
+  }
+
+  size_t i = 0;
+  while (i < primes.size()) {
+    // consider a number x. We know in the first case, x == 2, which is prime.
+    int x = primes[i];
+    
+    size_t j = i + 1;
+    while (j < primes.size()) {
+      if (primes[j] % x == 0) { // if this number is a multiple of x, eliminate it.
+        primes.erase(primes.begin() + j);
+      }
+      else j++; // else, move on to the next number
+    }
+
+    i++;
+  }
+
+  return primes;
 }
